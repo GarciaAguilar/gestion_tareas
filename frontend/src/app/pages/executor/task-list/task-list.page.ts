628140ccae47afa3fc7c3809dee6c6d045bf1c3a
Bingomar <ga19033@ues.edu.sv>
2025-06-22 00:14:28 -0600

    Mostrar tareas a ejecutores

--- frontend/src/app/pages/executor/task-list/task-list.page.ts
+++ frontend/src/app/pages/executor/task-list/task-list.page.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { TaskService } from '../../../services/task.service';
 
 @Component({
-  selector: 'app-task-list',
+  selector: 'app-executor-task-list',
   templateUrl: './task-list.page.html',
   styleUrls: ['./task-list.page.scss'],
 })
 export class TaskListPage implements OnInit {
+  tareas: any[] = [];
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
+    this.cargarTareas();
   }
 
+  cargarTareas() {
+    this.taskService.getTasks().subscribe({
+      next: res => {
+        this.tareas = res.data || [];
+      },
+      error: () => this.mostrarToast('Error al cargar tus tareas')
+    });
+  }
+
+  async mostrarToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+      position: 'top',
+      color: 'danger'
+    });
+    toast.present();
+  }
 }
