f8d70a7b056b1b224c7016705f753d635ab08569
Bingomar <ga19033@ues.edu.sv>
2025-06-21 22:28:26 -0600

    Creación proyecto Angular 17 + Ionic + Actualizacion de headers apis

--- backend/api/register.php
+++ backend/api/register.php
@@ -1,7 +1,13 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 require_once '../config/Database.php';
 require_once '../models/User.php';
 require_once '../helpers/response.php';
